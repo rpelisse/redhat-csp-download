a9acc4dd2a28b1e6e621611c678549e4b426ee53
Romain Pelisse <belaran@gmail.com>
2021-11-24 15:50:08 +0100

    fixup

--- build-collection.sh
+++ build-collection.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
+readonly GALAXY_YML='galaxy.yml'
+readonly UPSTREAM_NS='middleware_automation'
+readonly DOWNSTREAM_NS='redhat'
 
-echo 'Hello'
+sed -i "${GALAXY_YML}" \
+    -e "s/\(^namespace: \)${UPSTREAM_NS}/\1${DOWNSTREAM_NS}/"
+if [ -n "${RELEASE_VERSION}" ]; then
+  sed -i "${GALAXY_YML}" \
+      -e "s/^\(version: \).*$/\1\"${RELEASE_VERSION}\"/"
+fi
+git diff .
+ansible-galaxy collection build .
