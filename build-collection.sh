aced93fd412d628ff7a22cc3517fd69b539d22ae
Romain Pelisse <belaran@gmail.com>
2021-11-24 16:21:30 +0100

    fixup

--- build-collection.sh
+++ build-collection.sh
@@ -3,6 +3,9 @@ readonly GALAXY_YML='galaxy.yml'
 readonly UPSTREAM_NS='middleware_automation'
 readonly DOWNSTREAM_NS='redhat'
 
+pwd
+ls .
+
 sed -i "${GALAXY_YML}" \
     -e "s/\(^namespace: \)${UPSTREAM_NS}/\1${DOWNSTREAM_NS}/"
 if [ -n "${RELEASE_VERSION}" ]; then
