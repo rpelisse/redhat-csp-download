4509d4044930c63a5cca64db0b16b6674dbbe089
Romain Pelisse <belaran@gmail.com>
2021-11-24 16:43:16 +0100

    fixup

--- build-collection.sh
+++ build-collection.sh
@@ -4,16 +4,15 @@ readonly UPSTREAM_NS='middleware_automation'
 readonly DOWNSTREAM_NS='redhat'
 
 cd workdir
-echo "RELEASE_VERSION: ${RELEASE_VERSION}"
 echo -n "Change collection namespace from ${UPSTREAM_NS} to ${DOWNSTREAM_NS}..."
 sed -i "${GALAXY_YML}" \
     -e "s/\(^namespace: \)${UPSTREAM_NS}/\1${DOWNSTREAM_NS}/"
 echo 'Done.'
 
-if [ -n "${RELEASE_VERSION}" ]; then
-  echo -n "Bump version to ${RELEASE_VERSION}..."
+if [ -n "${VERSION}" ]; then
+  echo -n "Bump version to ${VERSION}..."
   sed -i "${GALAXY_YML}" \
-      -e "s/^\(version: \).*$/\1\"${RELEASE_VERSION}\"/"
+      -e "s/^\(version: \).*$/\1\"${VERSION}\"/"
   echo 'Done.'
 fi
 
